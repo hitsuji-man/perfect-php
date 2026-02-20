6e859edee2f0798a2301171bfd2f41711c9a763d
hitsuji-man <renton1q84@gmail.com>
2026-02-20 19:38:11 +0900

    ひとこと掲示板:投稿フォームと投稿処理

---
+++ chapter06/oneline_bbs/bbs.php
@@ -0,0 +1,87 @@
+<?php
+// データベースに接続
+$link = mysqli_connect('127.0.0.1', 'root', 'root', 'oneline_bbs',3306);
+if(!$link) {
+    die('データベースに接続できません: ' . mysqli_connect_error());
+}
+
+$errors = array();
+
+// POSTなら保存処理実行
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 名前が正しく入力されているかチェック
+    $name = null;
+    if (!isset($_POST['name']) || !strlen($_POST['name'])) {
+        $errors['name'] = '名前を入力してください';
+    } else if (strlen($_POST['name']) > 40) {
+        $errors['name'] = '名前は40文字以内で入力してください';
+    } else {
+        $name = $_POST['name'];
+    }
+
+    // ひとことが正しく入力されているかチェック
+    $comment = null;
+    if (!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+        $errors['comment'] = 'ひとことを入力してください';
+    } else if (strlen($_POST['comment']) > 200) {
+        $errors['comment'] = 'ひとことは200文字以内で入力してください';
+    } else {
+        $comment = $_POST['comment'];
+    }
+
+    // エラーがなければ保存
+    if (count($errors) === 0) {
+        // // 保存するためのsql文を作成
+        // $sql = "INSERT INTO `post` (`name`, `comment`, `created_at`) VALUES ('"
+        //     . mysqli_real_escape_string($link, $name) . "','"
+        //     . mysqli_real_escape_string($link, $comment) . "','"
+        //     . date('Y-m-d H:i:s') . "')";
+        
+        // // 保存する
+        // mysqli_query($link, $sql);
+
+        // プリペアドステートメントを用いる(推奨)
+        $stmt = mysqli_prepare(
+            $link,
+            "INSERT INTO post (name, comment, created_at) VALUES (?, ?, ?)"
+        );
+
+        if (!$stmt) {
+            die("Prepare failed: " . mysqli_error($link));
+        }
+
+        $created_at = date('Y-m-d H:i:s');
+
+        mysqli_stmt_bind_param($stmt, "sss", $name, $comment, $created_at);
+
+        mysqli_stmt_execute($stmt);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ひとこと掲示板</title>
+</head>
+<body>
+    <h1>ひとこと掲示板</h1>
+
+    <form action="bbs.php" method="post">
+        <?php if (count($errors)): ?>
+        <ul class="error_list">
+            <?php foreach ($errors as $error): ?>
+            <li>
+                <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+            </li>
+            <?php endforeach ?>
+        </ul>
+        <?php endif ?>
+        名前: <input type="text" name="name" /><br />
+        ひとこと: <input type="text" name="comment" size="60" /><br />
+        <input type="submit" name="submit" value="送信" />
+    </form>
+</body>
+</html>

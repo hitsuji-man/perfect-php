d8caa36c247bc7fd53d1d4cc5c104e6edd4e99b6
hitsuji-man <renton1q84@gmail.com>
2026-02-23 21:21:42 +0900

    ひとこと掲示板:リダイレクト設定

--- chapter06/oneline_bbs/bbs.php
+++ chapter06/oneline_bbs/bbs.php
@@ -55,6 +55,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         mysqli_stmt_bind_param($stmt, "sss", $name, $comment, $created_at);
 
         mysqli_stmt_execute($stmt);
+
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     }
 }
 ?>

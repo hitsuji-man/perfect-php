cf7d20632ef9a0f29e1700466d50537a8a2e4b3a
hitsuji-man <renton1q84@gmail.com>
2026-02-24 21:56:45 +0900

    ひとこと掲示板:ビューファイルを別に切り分け

--- chapter06/oneline_bbs/bbs.php
+++ chapter06/oneline_bbs/bbs.php
@@ -75,43 +75,6 @@ if ($result !== false && mysqli_num_rows($result)) {
 // 取得結果を解放して接続を閉じる
 mysqli_free_result($result);
 mysqli_close($link);
+
+include 'views/bbs_view.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ひとこと掲示板</title>
-</head>
-<body>
-    <h1>ひとこと掲示板</h1>
-
-    <form action="bbs.php" method="post">
-        <?php if (count($errors)): ?>
-        <ul class="error_list">
-            <?php foreach ($errors as $error): ?>
-            <li>
-                <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
-            </li>
-            <?php endforeach ?>
-        </ul>
-        <?php endif ?>
-        名前: <input type="text" name="name" /><br />
-        ひとこと: <input type="text" name="comment" size="60" /><br />
-        <input type="submit" name="submit" value="送信" />
-    </form>
-
-    <?php if(count($posts) > 0): ?>
-    <ul>
-        <?php foreach ($posts as $post): ?>
-        <li>
-            <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>
-            <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
-            - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-    <?php endif; ?>
-</body>
-</html>

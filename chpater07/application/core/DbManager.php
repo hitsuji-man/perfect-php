e74f914bc0c74ad7f1a5643c6b179c3f2f0866bc
hitsuji-man <renton1q84@gmail.com>
2026-03-09 22:24:21 +0900

    コメントの追加

--- chpater07/application/core/DbManager.php
+++ chpater07/application/core/DbManager.php
@@ -2,10 +2,12 @@
 /** 接続情報を管理するクラス */
 class DbManager 
 {
+    // PDOクラスのインスタンスを配列で保持する
     protected $connections = array();
 
     public function connect($name, $params)
     {
+        // $params配列から値を取り出す際にキーが存在するかのチェックをしないで済むようにする
         $params = array_merge(array(
             'dsn'       => null,
             'user'      => '',
@@ -13,6 +15,7 @@ class DbManager
             'options'   => array(),
         ), $params);
 
+        // PDOクラスのインスタンスを作成
         $con = new PDO(
             $params['dsn'],
             $params['user'],
@@ -20,11 +23,18 @@ class DbManager
             $params['options']
         );
 
+        // PDOの内部でエラーが起きた際に例外を発生させる
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $this->connections[$name] = $con;
     }
 
+    /**
+     * connect()メソッドで接続したコネクションを取得する
+     *
+     * @param string $name
+     * @return array|PDO
+     */
     public function getConnection($name = null)
     {
         if (is_null($name)) {

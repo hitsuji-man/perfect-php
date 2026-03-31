65ef7fa8867957a57ad03b2d414641344e42e003
hitsuji-man <renton1q84@gmail.com>
2026-03-31 20:18:50 +0900

    Applicationクラスから呼び出され、実際にアクションを実行するメソッド

--- chpater07/application/core/Controller.php
+++ chpater07/application/core/Controller.php
@@ -25,4 +25,31 @@ abstract class Controller
         $this->session     = $application->getSession();
         $this->db_manager  = $application->getDbManager();
     }
+
+    /**
+     * Applicationクラスから呼び出され、実際にアクションを実行
+     *
+     * @param string $action
+     * @param array $params
+     * @return string レスポンスとして返すコンテンツ
+     *
+     * @throws UnauthorizedActionException 認証が必須なアクションに認証前にアクセスした場合
+     */
+    public function run($action, $params = array())
+    {
+        $this->action_name = $action;
+
+        $action_method = $action . 'Action';
+        if (!method_exists($this, $action_method)) {
+            $this->forward404();
+        }
+
+        if ($this->needsAuthentication($action) && !$this->session->isAuthenticated()) {
+            throw new UnauthorizedActionException();
+        }
+
+        $content = $this->$action_method($params);
+
+        return $content;
+    }
 }

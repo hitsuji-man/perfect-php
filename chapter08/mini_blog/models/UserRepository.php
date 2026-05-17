55147b4866793edf06d467dcdf5a1f1f4e064995
hitsuji-man <renton1q84@gmail.com>
2026-05-17 17:46:37 +0900

    ユーザー登録判定の修正

--- chapter08/mini_blog/models/UserRepository.php
+++ chapter08/mini_blog/models/UserRepository.php
@@ -41,7 +41,7 @@ class UserRepository extends DbRepository
         $sql = "SELECT COUNT(id) as count FROM user WHERE user_name = :user_name";
 
         $row = $this->fetch($sql, array(':user_name' => $user_name));
-        if ($row['count'] === '0') {
+        if ($row['count'] === '0' || $row['count'] === 0) {
             return true;
         }
 
